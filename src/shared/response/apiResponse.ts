75b4bdd55f391e9a4bc6774b6f39d83aa61f03e9
Tran Quang Ha <tranquangha2006@gmail.com>
2026-09-16 12:57:30 +0700

    feat: update api response type

--- src/shared/response/apiResponse.ts
+++ src/shared/response/apiResponse.ts
@@ -1,9 +1,35 @@
 export class ApiResponse<T> {
-  constructor(
-    public status: number,
-    public success: boolean,
-    public data: T,
-    public message: string,
-    public timestamp: Date,
-  ) {}
+  private status: number;
+  private success: boolean;
+  private data: T;
+  private message: string;
+  private timestamp: Date;
+
+  constructor(status: number, success: boolean, data: T, message: string) {
+    this.status = status;
+    this.success = success;
+    this.data = data;
+    this.message = message;
+    this.timestamp = new Date();
+  }
+
+  public getStatus(): number {
+    return this.status;
+  }
+
+  public getSuccess(): boolean {
+    return this.success;
+  }
+
+  public getData(): T {
+    return this.data;
+  }
+
+  public getMessage(): string {
+    return this.message;
+  }
+
+  public getTimestamp(): Date {
+    return this.timestamp;
+  }
 }
